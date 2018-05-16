0958c516fc7ddf7a0322109d6de76fe90275e8c9
David Hewitt <david.hewitt@indigotree.co.uk>
2018-05-16 13:40:12 +0100

    Defer critical styles

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,24 @@
         document.body.classList[window.pageYOffset >= sticky ? 'add' : 'remove']('header--fixed')
     }
 
+    function loadDeferredStyles() {
+        var addStylesNode = document.getElementById("deferred-styles");
+        var replacement = document.createElement("div");
+        replacement.innerHTML = addStylesNode.textContent;
+        document.body.appendChild(replacement)
+        addStylesNode.parentElement.removeChild(addStylesNode);
+    }
+
+    var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+          window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    if (raf) {
+        raf(function() { 
+            window.setTimeout(loadDeferredStyles, 0);
+        });
+    } else {
+        window.addEventListener('load', loadDeferredStyles);
+    }
+
     $(function() {
         $('#carousel').slick({
             dots: false,
